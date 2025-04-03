af9a91067092326051dbc1de269c4079f3fd05f4
Alex <alexandrecarnerop@gmail.com>
2025-04-03 10:15:15 +0200

    Update for static site hosting

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   }
